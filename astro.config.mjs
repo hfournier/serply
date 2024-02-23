2a54efd81a8221279c73048ae8f6a4dedd2ea60a
Henri Fournier <hfournier@intratela.com>
2024-02-23 15:05:10 -0800

    First update adding home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon({
+      include: {
+        mdi: ["check", "close", "github", "menu"],
+        "fa6-solid": ["gauge"],
+        "heroicons-outline": ["support"],
+      },
+    }),
+  ],
+});
